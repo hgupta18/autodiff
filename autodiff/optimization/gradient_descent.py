daaca56c827a23eff9b723b23b4ef6fe964d19fa
CoopLo <cooper.lorsung@gmail.com>
2019-12-09 03:59:00 -0500

    Updated documentation for gradient descent algorithm

--- autodiff/optimization/gradient_descent.py
+++ autodiff/optimization/gradient_descent.py
@@ -5,46 +5,66 @@ import numpy as np
 from autodiff.autodiff import AutoDiff as ad
 
 
-def gradient_descent(f, x, steps=100000, step_size=0.1, tol=10e-8):
+def gradient_descent(func, num, step_size=0.1, tol=10e-8, max_iter=10000):
     '''
         INPUTS:
-          f - the function we would like to perform gradient descent on
-          x - vector of inputs as an AutoDiff object
-          step - step size of algorithm
-          tol - convergence critera. if 
+          func: callable (function)
+                 the function we would like to perform gradient descent on
+
+          num: np.ndarray of AutoDiff objects
+                vector of inputs a list of AutoDiff objects
+
+          step_size: float, optional (default = 0.1)
+                      step size of algorithm
+
+          tol: float, optional (default = 1e-10)
+                convergence critera. if 
+
+          max_iter: int, optional (default = 10000)
+                    number of iterations allowed before no convergence is declared
+
 
         OUTPUTS:
-          min - the minimum to which gradient descent converged
-          converged - Boolean indicating whether or not the method converged
+          min: np.ndarray of AutoDiff objects
+               the minimum to which gradient descent converged
+
+          converged: boolean
+                     True if the algorithm comverge, else False
+          iterations: int
+                      The number of iterations performed, whether or not the algorithm converged
 
         NOTE:
-          Need to catch failure modes
+          This algorithm terminates when the gradient is zero, whether or not that point
+          is a minimum or maximum. The algorithm is fairly robust and should converge
+          eventually, depending on step size. Default parameters are set so that most
+          functions used for input should converge.
     '''
 
-    n_func = len(x)
-    default_der = np.copy([x[i].der for i in range(n_func)])
-    last = np.copy(f(x).val)
+    # Set up values
+    n_func = len(num)  # Number of functions in the vector
+    default_der = np.copy([num[i].der for i in range(n_func)]) # Used for casting back to AD
+    last = np.copy(func(num).val)  # Last value, used to check convergence
+
     for i in range(steps):
 
         # Evaluate function to get derivatives
-        f_val = f(x)
+        f_val = func(num)
 
         # Update values by stepping along derivative
         for j in range(n_func):
-            x[j] -= ad(step_size*f_val.der[j])
+            num[j] -= ad(step_size*f_val.der[j])
 
         # Recast values as autodiff objects
         for j in range(n_func):
-            x[j] = ad(x[j].val, default_der[j])
+            num[j] = ad(num[j].val, default_der[j])
 
         # Check if converged
-        if(np.linalg.norm(x - last) < tol):
-            return [x, True, i]
+        if(np.linalg.norm(num - last) < tol):
+            return [num, True, i]
 
-        last = np.copy(x)
+        last = np.copy(num)
 
     return [x, False, i]
-
 
 
 if __name__ == '__main__':
