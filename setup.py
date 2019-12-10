8390117c1c5e381155e323e526d119a82b0d2c9d
Hardik Gupta <hardikgupta@g.harvard.edu>
2019-12-10 09:01:36 -0500

    updating travis and setup files

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
   name = 'autodiff-kgl',
   packages=setuptools.find_packages(), # packages = ['autodiff'],
   version = '0.3.3',
   license = 'MIT',
   description = 'Implementation of automatic differentiation using forward mode',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
   author = 'Jade Kessler, Hardik Gupta, and Cooper Lorsung',
   author_email = 'hardikgupta@g.harvard.edu',
   url = 'https://github.com/make-AD-ifference/cs207-FinalProject',
